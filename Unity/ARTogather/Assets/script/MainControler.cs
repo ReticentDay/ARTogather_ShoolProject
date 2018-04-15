7a28b227110dfc9a484875545c1fd07baeb6cd4b
Unknown <aa532199@gmail.com>
2018-04-15 16:45:57 +0800

    make the Stacked wood

---
+++ Unity/ARTogather/Assets/script/MainControler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainControler : MonoBehaviour {
+    public bool HitOnTrue;
+	// Use this for initialization
+	void Start () {
+        HitOnTrue = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReadyToAdd()
+    {
+        Debug.Log("Button");
+        HitOnTrue = true;
+    }
+
+    public void AddClub(GameObject point)
+    {
+        point.GetComponent<PointScript>().Create();
+    }
+}
