68e4ff5d31ba5a95cf602a14dd0a17f6265371ba
a104590019 <andylaw10108@gmail.com>
2018-04-17 16:49:44 +0800

    socket multi connect

---
+++ clienttest/clienttest/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace clienttest
+{
+    class Program
+    {
+        //宣告網路資料流變數
+        NetworkStream myNetworkStream;
+        //宣告 Tcp 用戶端物件
+        TcpClient myTcpClient;
+
+        static void Main(string[] args)
+        {
+            Program myNetworkClient = new Program();
+
+            //取得主機名稱
+            string hostName ="127.0.0.1";
+            //取得連線 IP 位址
+            int connectPort = 36000;
+            //建立 TcpClient 物件
+            myNetworkClient.myTcpClient = new TcpClient();
+            try
+            {
+                //測試連線至遠端主機
+                myNetworkClient.myTcpClient.Connect(hostName, connectPort);
+                Console.WriteLine("連線成功 !!\n");
+            }
+            catch
+            {
+                Console.WriteLine
+                           ("主機 {0} 通訊埠 {1} 無法連接  !!", hostName, connectPort);
+                
+            }
+            while(true)
+            {
+                String strings = Console.ReadLine();
+                if(strings == "Exit")
+                {
+                    break;
+                }
+                myNetworkClient.WriteData(strings);
+                myNetworkClient.ReadData();
+            }
+        }
+
+        //寫入資料
+        void WriteData(String strTest)
+        {
+            //將字串轉 byte 陣列，使用 ASCII 編碼
+            Byte[] myBytes = Encoding.ASCII.GetBytes(strTest);
+
+            Console.WriteLine("建立網路資料流 !!");
+            //建立網路資料流
+            myNetworkStream = myTcpClient.GetStream();
+
+            Console.WriteLine("將字串寫入資料流　!!");
+            //將字串寫入資料流
+            myNetworkStream.Write(myBytes, 0, myBytes.Length);
+        }
+
+        //讀取資料
+        void ReadData()
+        {
+            Console.WriteLine("從網路資料流讀取資料 !!");
+            //從網路資料流讀取資料
+            int bufferSize = myTcpClient.ReceiveBufferSize;
+            byte[] myBufferBytes = new byte[bufferSize];
+            myNetworkStream.Read(myBufferBytes, 0, bufferSize);
+            //取得資料並且解碼文字
+            Console.WriteLine(Encoding.ASCII.GetString(myBufferBytes, 0, bufferSize));
+        }
+    }
+}
---
+++ networktest/networktest/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Text;
+
+namespace csharp_multi_threaded_server_socket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Net.IPAddress theIPAddress;
+            //建立 IPAddress 物件(本機)
+            theIPAddress = System.Net.IPAddress.Parse("127.0.0.1");
+            //建立監聽物件
+            TcpListener myTcpListener = new TcpListener(theIPAddress, 36000);
+            //啟動監聽
+            myTcpListener.Start();
+            Console.WriteLine("通訊埠 36000 等待用戶端連線...... !!");
+            int counter = 0;
+            do
+            {
+                Socket mySocket = myTcpListener.AcceptSocket();
+                try
+                {
+                    //偵測是否有來自用戶端的連線要求，若是
+                    //用戶端請求連線成功，就會秀出訊息。
+                    if (mySocket.Connected)
+                    {
+                        counter++;
+                        Console.WriteLine("用戶(" + counter + ")連線成功 !!");
+                        handleClient client = new handleClient();
+                        client.startClient(mySocket,counter);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    mySocket.Close();
+                    Console.WriteLine("通訊埠 36000 等待用戶端連線...... !!");
+                    mySocket = myTcpListener.AcceptSocket();
+                }
+
+            } while (true);
+        }
+    }
+    public class handleClient
+    {
+        Socket clientSocket;
+        int No;
+        public void startClient(Socket clientSocket,int No)
+        {
+            this.clientSocket = clientSocket;
+            this.No = No;
+            Thread clientThread = new Thread(reply);
+            clientThread.Start();
+        }
+        private void reply()
+        {
+            while (true) {
+                try
+                {
+                    int dataLength;
+                    byte[] myBufferBytes = new byte[1000];
+                    //取得用戶端寫入的資料
+                    dataLength = clientSocket.Receive(myBufferBytes);
+
+                    Console.WriteLine("接收到的資料長度 {0} \n ", dataLength.ToString());
+                    Console.WriteLine("取出用戶端寫入網路資料流的資料內容 :");
+                    Console.WriteLine(Encoding.ASCII.GetString(myBufferBytes, 0, dataLength) + "\n");
+                    Console.WriteLine("按下 [任意鍵] 將資料回傳至用戶端 !!");
+                    Console.ReadLine();
+                    //將接收到的資料回傳給用戶端
+                    clientSocket.Send(myBufferBytes, dataLength, 0);
+                    Console.WriteLine("已傳回資料");
+                }
+                catch (Exception e)
+                {
+                    clientSocket.Close();
+                    Console.WriteLine("用戶("+No+")已中斷連線");
+                    Console.WriteLine("通訊埠 36000 等待用戶端連線...... !!");
+                    break;
+                }
+            }
+        }
+    }
+}
